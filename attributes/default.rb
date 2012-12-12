dd80a9294666a250ea202509b2b5d2d5597c02ec
Stephen Lauck <stpehen.lauck@gmail.com>
2012-12-11 16:23:29 -0800

    add platform checks for attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,26 @@
-if node['memory'] && node['memory']['total']
-  # Set memory to 80% of total
-  default[:memcached][:memory] = (node['memory']['total'] * 0.8).ceil
-else
-  default[:memcached][:memory] = 64
+case platform
+when "ubuntu"
+	default[:memcached][:memory]  = 64
+	default[:memcached][:port]    = 11211
+	default[:memcached][:user]    = "nobody"
+	default[:memcached][:listen]  = "0.0.0.0"
+	default[:memcached][:maxconn] = "1024"
+when "smartos"
+	if node['memory'] && node['memory']['total']
+	  # Set memory to 80% of total
+	  default[:memcached][:memory] = (node['memory']['total'] * 0.8).ceil
+	else
+	  default[:memcached][:memory] = 64
+	end
+
+	default[:memcached][:port] = 11211
+	default[:memcached][:user] = "nobody"
+	default[:memcached][:listen] = "0.0.0.0"
+	default[:memcached][:maxconn] = "1024"
+
+	# Setting for performance
+	default[:memcached][:large_pages] = true
+	default[:memcached][:slab_page_size] = "1m"
 end
 
-default[:memcached][:port] = 11211
-default[:memcached][:user] = "nobody"
-default[:memcached][:listen] = "0.0.0.0"
-default[:memcached][:maxconn] = "1024"
 
-# Setting for performance
-default[:memcached][:large_pages] = true
-default[:memcached][:slab_page_size] = "1m"
